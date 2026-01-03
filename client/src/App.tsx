65907fe65df062f83745820ea0490ca01be79014
wprospere <wp@cloudcarsltd.com>
2026-01-03 01:08:24 +0000

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,15 +17,9 @@ import ImageManager from "./pages/admin/ImageManager";
 import AdminSettings from "./pages/admin/Settings";
 import Inquiries from "./pages/admin/Inquiries";
 import TeamMembers from "./pages/admin/TeamMembers";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import DriverOnboardingPage from "@/pages/DriverOnboarding";
 import DriverOnboardingReview from "@/pages/admin/DriverOnboardingReview";
-
-// ...inside your router:
-<Routes>
-  {/* other routes... */}
-  <Route path="/driver/onboarding" element={<DriverOnboardingPage />} />
-</Routes>
 
 function Router() {
   return (
@@ -35,18 +29,21 @@ function Router() {
       <Route path="/privacy" component={Privacy} />
       <Route path="/cookies" component={Cookies} />
 
-      {/* Admin Routes */}
+      {/* Driver onboarding (public) */}
+      <Route path="/driver/onboarding" component={DriverOnboardingPage} />
+
+      {/* Admin routes */}
       <Route path="/admin/login" component={AdminLoginPage} />
       <Route path="/admin/inquiries" component={Inquiries} />
       <Route path="/admin/team-members" component={TeamMembers} />
       <Route path="/admin/content" component={ContentEditor} />
       <Route path="/admin/images" component={ImageManager} />
       <Route path="/admin/settings" component={AdminSettings} />
+
+      {/* Review onboarding docs (admin) */}
+      <Route path="/admin/driver-onboarding/:id" component={DriverOnboardingReview} />
+
       <Route path="/admin" component={AdminDashboard} />
-<Route
-  path="/admin/driver-onboarding/:id"
-  element={<DriverOnboardingReview />}
-/>
 
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
@@ -54,7 +51,7 @@ function Router() {
   );
 }
 
-function App() {
+export default function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider defaultTheme="dark">
@@ -66,14 +63,3 @@ function App() {
     </ErrorBoundary>
   );
 }
-
-export default App;
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DriverOnboardingPage from "@/pages/DriverOnboarding";
-
-// ...inside your router:
-<Routes>
-  {/* other routes... */}
-  <Route path="/driver/onboarding" element={<DriverOnboardingPage />} />
-</Routes>
-
